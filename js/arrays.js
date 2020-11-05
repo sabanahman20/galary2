49f9b68749a1e12486fc63ff2fae9b869c6a26da
Evgenia Alievsky <daniel.alievsky@gmail.com>
2020-11-05 16:58:51 +0200

    добавили вёрстку картинок

--- js/arrays.js
+++ js/arrays.js
@@ -1,14 +1,5 @@
 window.addEventListener("load", function(){
 	var click = document.querySelector("#clicker");
-	var testarray = document.querySelectorAll(".test");
-    click = addEventListener("click",function(){
-	for(var i = 0;i < testarray.length;i++){
-		testarray[i].style.backgroundColor = "green";
-	}
-	/*for(var i = testarray.length-1;i > -1;i--){
-		testarray[i].style.backgroundColor = "green";
-	}*/
-	});
- });
-
+	var
+});
 
